d537e4e389682ab647f30453bb0ab4c8127f734b
Gerald <geraldtrogado@gmail.com>
2025-03-19 02:08:30 +0800

    added user input and input validation to remove leading spaces from user input

--- remove_leading_spaces.py
+++ remove_leading_spaces.py
@@ -3,3 +3,17 @@
 # 2. Remove leading spaces
 # 3. Print the cleaned name
 
+def remove_leading_spaces():
+    while True:
+        name_input = input("Enter your full name: ")
+        contains_valid_current_characteracter = False
+        
+    #Check if input has at least one non-space character
+    for current_character in name_input:
+            if current_character != ' ':
+                has_text = True
+                break
+            
+    if has_text:
+        break
+    print("Error: Name cannot be only spaces.")
