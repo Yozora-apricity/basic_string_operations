809f5d13b7f8f62fdc925905cd9bef9f9e87625d
Gerald <geraldtrogado@gmail.com>
2025-03-19 02:38:01 +0800

    Added the function where it ask user to enter a number and added error handling when invalid number is inputted

--- format_six_digits.py
+++ format_six_digits.py
@@ -2,3 +2,15 @@
 # 1. Get a number from the user
 # 2. Validate the range
 # 3. Format and print the number
+
+def format_six_digits():
+    while True:
+        try:
+            user_number_input = input("Enter a number (0-1000): ")
+            converted_integer_number = int(user_number_input)
+            
+            if 0 <= converted_integer_number <= 1000:
+                break
+            print("Error: Number out of range. Try again.")
+        except ValueError:
+            print("Error: Invalid input. Enter a valid number.")
