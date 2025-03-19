2e25834301ffabbe797d2979413617eb810c3fba
Gerald <geraldtrogado@gmail.com>
2025-03-19 23:25:01 +0800

    Added pseudo code to 3 files

---
+++ convert_to_pascal_case.py
@@ -0,0 +1,4 @@
+# Pseudo code:
+# 1. Get user input
+# 2. Convert input to pascal case
+# 3. Print the pascal case name
---
+++ convert_to_snake_case.py
@@ -0,0 +1,4 @@
+# Pseudo code:
+# 1. Get user input
+# 2. Convert input to snakecase
+# 3. Print the snakecase name
