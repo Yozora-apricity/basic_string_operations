3a1f730cda2fc2bcffb7c9c24f047c458d456491
Gerald <geraldtrogado@gmail.com>
2025-03-19 23:37:26 +0800

    Added functionality to convert user input to pascal case and snake case, including user prompts and output display

--- convert_to_pascal_case.py
+++ convert_to_pascal_case.py
@@ -2,3 +2,7 @@
 # 1. Get user input
 # 2. Convert input to pascal case
 # 3. Print the pascal case name
+
+fullname = input("Enter your full name: ")
+pascal_case_name = fullname.title().replace(" ", "")
+print("Output:", pascal_case_name)
--- convert_to_snake_case.py
+++ convert_to_snake_case.py
@@ -2,3 +2,7 @@
 # 1. Get user input
 # 2. Convert input to snakecase
 # 3. Print the snakecase name
+
+fullname = input("Enter your full name: ")
+snake_case_name = fullname.lower().replace(" ", "")
+print("Output:", snake_case_name)
