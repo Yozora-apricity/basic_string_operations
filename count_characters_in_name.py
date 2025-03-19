ef9affc2deb0edd83ffb582f0e245e424902f91e
Gerald <geraldtrogado@gmail.com>
2025-03-19 23:50:02 +0800

    Added functionality to count characters in user input and print the result

--- count_characters_in_name.py
+++ count_characters_in_name.py
@@ -2,3 +2,7 @@
 # 1. Get user input
 # 2. Count the number of characters (including spaces)
 # 3. Print the Output
+
+fullname = input("Enter your full name: ")
+char_count = len(fullname)
+print(char_count)
