05d646a60438403aec982c8332301f84f638004e
Gerald <geraldtrogado@gmail.com>
2025-03-19 23:14:16 +0800

    Added functionality to convert user input to lowercase and display it

--- convert_to_lowercase.py
+++ convert_to_lowercase.py
@@ -2,3 +2,7 @@
 # 1. Get user input
 # 2. Convert input to lowercase
 # 3. Print the lowercase name
+
+fullname = input("Enter your full name: ")
+lowercase_name = fullname.lower()
+print("Your full name in lowercase is: ", lowercase_name)
