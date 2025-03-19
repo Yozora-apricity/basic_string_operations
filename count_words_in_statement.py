756ad248dca835b1fb16caa92d30b6cf7894d874
Gerald <geraldtrogado@gmail.com>
2025-03-19 23:45:50 +0800

    Added functionality to count words in user input and print the result

--- count_words_in_statement.py
+++ count_words_in_statement.py
@@ -2,3 +2,7 @@
 # 1. Get user input
 # 2. Split the statement into words and count them
 # 3. Print the output
+
+statement = input("Enter your full name: ")
+word_count = len(statement.split())
+print(word_count)
