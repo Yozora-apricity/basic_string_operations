4fc57b5fb8733d4b06c6e9ffea6618c425f5dc37
Gerald <geraldtrogado@gmail.com>
2025-03-19 23:33:59 +0800

    Added a functionality that convert user input to proper case and display it

--- convert_to_proper_case.py
+++ convert_to_proper_case.py
@@ -2,3 +2,7 @@
 # 1. Get user input
 # 2. Convert input to propercase
 # 3. Print the propercase name
+
+fullname = input("Enter your full name: ")
+proper_name = fullname.lower().title()
+print("Your propercase name is:", proper_name)
