0a0965022fd8bdb65890c6ec0ffbced6e18ee79b
Gerald <geraldtrogado@gmail.com>
2025-03-19 23:49:28 +0800

    Added functionality to reverse the character case of user input and print the result

--- reverse_character_case.py
+++ reverse_character_case.py
@@ -2,3 +2,7 @@
 # 1. Get user input
 # 2. Convert each character to the opposite case
 # 3. Print the output in reverse
+
+fullname = input("Enter your full name: ")
+reverse_casing_name = fullname.swapcase()
+print(reverse_casing_name)
